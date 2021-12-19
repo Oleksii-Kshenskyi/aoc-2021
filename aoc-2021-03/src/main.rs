61522c93a2c070d59d9d6009d85a5d39220c6c26
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2021-12-19 13:55:41 +0200

    Day 3, part 2 botched: example is calculated correctly, actual input calculation errors out.

--- aoc-2021-03/src/main.rs
+++ aoc-2021-03/src/main.rs
@@ -10,6 +10,8 @@ struct DiagnosticReport {
     data: Array2D<char>,
     gamma: String,
     epsilon: String,
+    oxygen: String,
+    co2: String,
 }
 
 impl DiagnosticReport {
@@ -18,9 +20,13 @@ impl DiagnosticReport {
             data: DiagnosticReport::convert_strvec_to_array2d(input),
             gamma: String::new(),
             epsilon: String::new(),
+            oxygen: String::new(),
+            co2: String::new(),
         };
         new_self.calculate_gamma();
         new_self.calculate_epsilon();
+        new_self.calculate_oxygen();
+        new_self.calculate_co2();
 
         new_self
     }
@@ -31,11 +37,19 @@ impl DiagnosticReport {
     pub fn get_epsilon(&self) -> usize {
         usize::from_str_radix(&self.epsilon, 2).expect("Can't convert epsilong string to a binary number.")
     }
+    pub fn get_oxygen(&self) -> usize {
+        usize::from_str_radix(&self.oxygen, 2).expect("Can't convert oxygen rate to a binary number.")
+    }
+    pub fn get_co2(&self) -> usize {
+        usize::from_str_radix(&self.co2, 2).expect("Can't convert co2 scrubber rating to a binary number.")
+    }
 
     fn convert_strvec_to_array2d(strvec: &Vec<String>) -> Array2D<char> {
         let mut vecvecchars: Vec<Vec<char>> = vec![];
         for an_str in strvec {
-            vecvecchars.push(an_str.chars().collect::<Vec<char>>());
+            if !an_str.chars().all(|c| c.is_whitespace()) {
+                vecvecchars.push(an_str.chars().collect::<Vec<char>>());
+            }
         }
 
         Array2D::from_rows(vecvecchars.as_slice())
@@ -79,6 +93,113 @@ impl DiagnosticReport {
 
         self.epsilon = new_epsilon;
     }
+
+    fn calculate_oxygen(&mut self) {
+        let mut new_oxygen = String::new();
+        let mut filtered_data = self.data.clone();
+        for column_index in 0..filtered_data.row_len() {
+            let mut zeros_count = 0 as usize;
+            let mut ones_count = 0 as usize;
+            for row_index in 0..filtered_data.column_len() {
+                match filtered_data[(row_index, column_index)] {
+                    '0' => zeros_count += 1,
+                    '1' => ones_count += 1,
+                    _ => panic!("ERROR: input has some other character but 1 or 0."),
+                }
+            }
+            match ones_count.cmp(&zeros_count) {
+                Ordering::Less => Self::preserve_zeros_at_pos(&mut filtered_data, column_index),
+                Ordering::Greater => Self::preserve_ones_at_pos(&mut filtered_data, column_index),
+                Ordering::Equal => {
+                    new_oxygen = Self::preserve_the_one_at_pos(&filtered_data, column_index);
+                },
+            }
+        }
+
+        self.oxygen = new_oxygen;
+    }
+
+    fn calculate_co2(&mut self) {
+        let mut new_co2 = String::new();
+        let mut filtered_data = self.data.clone();
+        for column_index in 0..filtered_data.row_len() - 1 {
+            let mut zeros_count = 0 as usize;
+            let mut ones_count = 0 as usize;
+            for row_index in 0..filtered_data.column_len() {
+                match filtered_data[(row_index, column_index)] {
+                    '0' => zeros_count += 1,
+                    '1' => ones_count += 1,
+                    _ => panic!("ERROR: input has some other character but 1 or 0."),
+                }
+            }
+            match ones_count.cmp(&zeros_count) {
+                Ordering::Less => Self::preserve_ones_at_pos(&mut filtered_data, column_index),
+                Ordering::Greater => Self::preserve_zeros_at_pos(&mut filtered_data, column_index),
+                Ordering::Equal => {
+                    new_co2 = Self::preserve_the_zero_at_pos(&filtered_data, column_index);
+                },
+            }
+        }
+
+        self.co2 = new_co2;
+    }
+
+    fn preserve_zeros_at_pos(data: &mut Array2D<char>, pos: usize) {
+        let mut new_data: Vec<Vec<char>> = vec![];
+        for (row_index, _row) in data.rows_iter().enumerate() {
+            if data[(row_index, pos)] == '0' {
+                new_data.push(Self::row_to_vec_at_index(&data, row_index));
+            }
+        }
+        *data = Self::vecvec_to_array2d(&new_data);
+    }
+    fn preserve_ones_at_pos(data: &mut Array2D<char>, pos: usize) {
+        let mut new_data: Vec<Vec<char>> = vec![];
+        for (row_index, _row) in data.rows_iter().enumerate() {
+            if data[(row_index, pos)] == '1' {
+                new_data.push(Self::row_to_vec_at_index(&data, row_index));
+            }
+        }
+        *data = Self::vecvec_to_array2d(&new_data);
+    }
+    fn preserve_the_one_at_pos(data: &Array2D<char>, index: usize) -> String {
+        if data.column_len() != 2 {
+            panic!("The number of rows is different from 2 on an iteration where there are two items left in binary search. Something went wrong.");
+        }
+        let mut preserved = String::new();
+
+        let preserved_row_index: usize = if data[(0,index)] == '1' { 0 } else { 1 };
+        for (column_index, _column) in data.columns_iter().enumerate() {
+            preserved.push(data[(preserved_row_index,column_index)]);
+        }
+
+        preserved
+    }
+    fn preserve_the_zero_at_pos(data: &Array2D<char>, index: usize) -> String {
+        if data.column_len() != 2 {
+            panic!("The number of rows is different from 2 on an iteration where there are two items left in binary search. Something went wrong.");
+        }
+        let mut preserved = String::new();
+
+        let preserved_row_index: usize = if data[(0,index)] == '0' { 0 } else { 1 };
+        for (column_index, _column) in data.columns_iter().enumerate() {
+            preserved.push(data[(preserved_row_index,column_index)]);
+        }
+
+        preserved
+    }
+
+    fn row_to_vec_at_index(data: &Array2D<char>, row_index: usize) -> Vec<char> {
+        let mut row_vec: Vec<char> = vec![];
+        for (column_index, _column) in data.columns_iter().enumerate() {
+            row_vec.push(data[(row_index, column_index)]);
+        }
+
+        row_vec
+    }
+    fn vecvec_to_array2d(vecvec: &Vec<Vec<char>>) -> Array2D<char> {
+        Array2D::from_rows(vecvec.as_slice())
+    }
 }
 
 fn main() {
@@ -101,4 +222,7 @@ fn main() {
     let diag_data = DiagnosticReport::new(&input);
     let part1_answer = diag_data.get_gamma() * diag_data.get_epsilon();
     println!("Part 1 answer is: {}.", part1_answer);
+
+    let part2_answer = diag_data.get_oxygen() * diag_data.get_co2();
+    println!("Part 2 answer is: {}.", part2_answer);
 }
