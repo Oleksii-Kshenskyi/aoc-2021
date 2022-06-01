95c9856dcc6dcfe016e89e860ccad660f795a919
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-06-01 20:13:05 +0300

    Clojure: Day 3, part 1 done.

--- clojure/day_three/src/day_three/core.clj
+++ clojure/day_three/src/day_three/core.clj
@@ -4,24 +4,45 @@
 (require '[clojure.java.io :as io]
          '[clojure.string :as str])
 
+(defn sort-pair [pair] [(get pair \0) (get pair \1)])
+(defn update-gamma [gamma pair]
+  (if (> (nth pair 0) (nth pair 1))
+    (str gamma \0)
+    (str gamma \1)))
+(defn get-freqs [matrix]
+  (->> matrix
+       (apply map vector)
+       (map frequencies)
+       (map sort-pair)
+       (into [])))
+(defn get-gamma [matrix]
+  (let [freqs (get-freqs matrix)]
+    (loop [index 0
+           gamma ""]
+      (if (= index (count freqs))
+        gamma
+        (recur (inc index) (update-gamma gamma (nth freqs index)))))))
+(defn get-epsilon [gamma] (apply str (map {\0 \1 \1 \0} gamma)))
+
 (defn split-input [streeng] (str/split streeng #"\s+"))
-(defn str->int [streeng] (Integer/parseInt streeng))
-(defn into-ints [streengs] (map str->int streengs))
 
-(defn part-one [ints] 0)
-(defn part-two [ints] 0)
+(defn part-one [matrix]
+  (let [gamma (get-gamma matrix)
+        gval (Integer/parseInt gamma 2)
+        eval (Integer/parseInt (get-epsilon gamma) 2)]
+    (* gval eval)))
+(defn part-two [matrix] 0)
 
-(defn get-ints [args]
+(defn get-matrix [args]
   (->> (nth args 0)
        (slurp)
-       (split-input)
-       (into-ints)))
+       (split-input)))
 (defn file-exists [filename] (.exists (io/as-file filename)))
 
 (defn -main [& args]
   (if (and (= (count args) 1) (file-exists (nth args 0)))
-    (let [ints (get-ints args)]
-      (println "Part 1 result: " (part-one ints))
-      (println "Part 2 result: " (part-two ints)))
+    (let [matrix (get-matrix args)]
+      (println "Part 1 result: " (part-one matrix))
+      (println "Part 2 result: " (part-two matrix)))
 
     (println "Need exactly ONE argument: an existing file name.")))
