cebdcacbc4acc9d84253ab1b2c876a6d701055ad
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-06-02 20:49:45 +0300

    Clojure: Day 3 part 2 finally working properly. Took 6 months lol.

--- clojure/day_three/src/day_three/core.clj
+++ clojure/day_three/src/day_three/core.clj
@@ -5,21 +5,45 @@
          '[clojure.string :as str])
 
 (defn sort-pair [pair] [(get pair \0) (get pair \1)])
-(defn get-char [strings row col] (nth (nth strings row) col))
-(defn sort-pair-v2 [pair c] 
-  (cond 
-    (= (get-char pair 0 (- (count (nth pair 0)) 1)) c) (nth pair 0)
-    (= (get-char pair 1 (- (count (nth pair 0)) 1)) c) (nth pair 1)))
+
+(defn split-input [streeng] (str/split streeng #"\s+"))
+
+(defn freq-row [matrix pos-in-row]
+  (let [zeros (filter #(= (nth % pos-in-row) \0) matrix)
+        ones (filter #(= (nth % pos-in-row) \1) matrix)]
+  (vector (count zeros) (count ones))))
+(defn compare-freqs [freqs end-char]
+  (cond
+    (= (nth freqs 0) (nth freqs 1)) :equal
+    (and (= end-char \1) (> (nth freqs 1) (nth freqs 0))) :ones
+    (and (= end-char \1) (> (nth freqs 0) (nth freqs 1))) :zeroes
+    (and (= end-char \0) (< (nth freqs 1) (nth freqs 0))) :ones
+    (and (= end-char \0) (< (nth freqs 0) (nth freqs 1))) :zeroes
+    :else (throw (Throwable. (str "Freqs comparison failed: " freqs)))))
+
+(defn filter-once [matrix filter-char pos-in-row]
+  (filter #(= filter-char (nth % pos-in-row)) matrix))
+
+(defn filter-matrix [matrix end-char pos-in-row]
+  (let [freqs (compare-freqs (freq-row matrix pos-in-row) end-char)]
+    (case freqs
+      :equal (filter-once matrix end-char pos-in-row)
+      :zeroes (filter-once matrix \0 pos-in-row)
+      :ones (filter-once matrix \1 pos-in-row))))
+
+
 (defn update-gamma [gamma pair]
   (if (> (nth pair 0) (nth pair 1))
     (str gamma \0)
     (str gamma \1)))
+
 (defn get-freqs [matrix]
   (->> matrix
        (apply map vector)
        (map frequencies)
        (map sort-pair)
        (into [])))
+
 (defn get-gamma [matrix]
   (let [freqs (get-freqs matrix)]
     (loop [index 0
@@ -27,30 +51,31 @@
       (if (= index (count freqs))
         gamma
         (recur (inc index) (update-gamma gamma (nth freqs index)))))))
+
 (defn get-epsilon [gamma] (apply str (map {\0 \1 \1 \0} gamma)))
 
-(defn get-part2-value [matrix search-string end-char]
+
+(defn get-part2-value [matrix end-char]
     (loop [filter-me matrix
            search-index 0]
-      (if (= (count filter-me) 2)
-        (sort-pair-v2 filter-me end-char)
-        (recur (filter #(= (nth search-string search-index) (nth % search-index)) filter-me) (inc search-index)))
-      ))
+      (if (= (count filter-me) 1)
+        (nth filter-me 0)
+        (recur (filter-matrix filter-me end-char search-index) (inc search-index)))))
 
-(defn get-oxygen-generator [matrix gamma] (get-part2-value matrix gamma \1))
-(defn get-co2-scrubber [matrix gamma] (get-part2-value matrix (get-epsilon gamma) \0))
+(defn get-oxygen-generator [matrix] (get-part2-value matrix \1))
 
-(defn split-input [streeng] (str/split streeng #"\s+"))
+(defn get-co2-scrubber [matrix] (get-part2-value matrix \0))
+
 
 (defn part-one [matrix]
   (let [gamma (get-gamma matrix)
         gval (Integer/parseInt gamma 2)
         eval (Integer/parseInt (get-epsilon gamma) 2)]
     (* gval eval)))
+
 (defn part-two [matrix]
-  (let [gamma (get-gamma matrix)
-        scrub (Integer/parseInt (get-co2-scrubber matrix gamma) 2)
-        oxygen (Integer/parseInt (get-oxygen-generator matrix gamma) 2)]
+  (let [scrub (Integer/parseInt (get-co2-scrubber matrix) 2)
+        oxygen (Integer/parseInt (get-oxygen-generator matrix) 2)]
     (* scrub oxygen)))
 
 (defn get-matrix [args]
