5ae0417deca775882e264306dda604ea4e210421
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-06-03 04:56:43 +0300

    Clojure: Day 4, part 1 botched.

--- clojure/day_four/src/day_four/core.clj
+++ clojure/day_four/src/day_four/core.clj
@@ -6,17 +6,63 @@
 
 (defn split-input [streeng] (s/split streeng #"\s+"))
 (defn str->int [streeng] (Integer/parseInt streeng))
-(defn into-ints [streengs] (map str->int streengs))
-(defn nth-last [index sequence] (nth sequence index))
 (defn drop-empty-strings [coll] (into [] (filter #(false? (empty? %)) coll)))
+(defn init-unmarked [coll] (into [] (map #(vector % :unmarked) coll)))
+(defn reconstruct [boards]
+  (->> boards
+       (partition 2)
+       (partition 5)
+       (partition 5)))
+(defn check-and-mark-single [flat-boards mark-number index]
+  (let [flatvec (into [] flat-boards)]
+  (if (= mark-number (nth flatvec index))
+    (assoc flatvec (+ index 1) :marked)
+    flat-boards)))
+(defn mark-with [mark-number boards]
+  (loop [flat (flatten boards)
+         index 0]
+    (if (> index (- (count flat) 2))
+      (reconstruct boards)
+      (recur (check-and-mark-single flat mark-number index) (+ index 2)))))
+(defn check-and-inc [sum pair]
+  (if (= (nth pair 1) :unmarked)
+    (+ sum (str->int (nth pair 0)))
+    sum))
+(defn unmarked-sum [board]
+  (loop [flat-board (flatten board)
+         sum 0]
+    (if (empty? flat-board)
+      sum
+      (recur (drop 2 flat-board) (check-and-inc sum (take 2 flat-board))))))
+(defn is-winning-row? [row]
+  (every? #(= (nth % 1) :marked) row))
+(defn any-winning-rows? [board]
+  (some true? (map is-winning-row? board)))
+(defn any-winning-cols? [board]
+  (any-winning-rows? (apply map vector board)))
+(defn is-winning-board? [board]
+  (or (any-winning-rows? board) (any-winning-cols? board)))
+(defn winning-board-exists [boards]
+  (loop [current boards]
+    (cond
+      (is-winning-board? (first current)) (first current)
+      (= (count current) 1) nil
+      :else (recur (drop 1 current)))))
 
-(defn part-one [ints] 0)
-(defn part-two [ints] 0)
+(defn part-one [game]
+  (loop [draw-seq (get game "draw")
+         boards (get game "boards")]
+    (let [winning-board (winning-board-exists boards)]
+    (if winning-board
+      (* (unmarked-sum winning-board) (first draw-seq))
+      (recur (drop 1 draw-seq) (mark-with (first draw-seq) boards))))))
+(defn part-two [game] 0)
 
 (defn split-board [board]
   (->> board
        (map split-input)
        (map drop-empty-strings)
+       (map init-unmarked)
        (into [])))
 (defn boards-from-lines [lines]
   (->> lines
