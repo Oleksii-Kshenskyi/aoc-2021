6ba766f65bd3504497dec50960514a263ce02038
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-06-06 13:15:19 +0300

    Clojure: Day 6 part 1, straightforwards solution. Works, but is too slow for actual input. Have to optimize.

--- clojure/day_six/src/day_six/core.clj
+++ clojure/day_six/src/day_six/core.clj
@@ -4,14 +4,31 @@
 (require '[clojure.java.io :as io]
          '[clojure.string :as str])
 
-(defn split-input [streeng] (str/split streeng #"\s+"))
+(defn split-input [streeng] (str/split streeng #"\,"))
 (defn str->int [streeng] (Integer/parseInt streeng))
-(defn into-ints [streengs] (map str->int streengs))
+(defn into-ints [streengs] (->> streengs (map str->int) (into [])))
+(defn dec-vec-at [vec index] (assoc vec index (dec (nth vec index))))
 
-(defn part-one [ints] 0)
-(defn part-two [ints] 0)
+(defn spawn-or-leave [fish index]
+  (let [fishvec (into [] fish)]
+    (if (= (nth fishvec index) -1)
+      (conj (assoc fishvec index 6) 8)
+      fishvec)))
+(defn single-simulate-spawns [fish]
+  (loop [index 0
+         result fish]
+    (if (= index (count fish))
+           result
+           (recur (inc index) (spawn-or-leave result index)))))
+(defn simulate-single-day [fish]
+  (let [stepped-fish (map dec fish)]
+    (single-simulate-spawns stepped-fish)))
 
-(defn get-ints [args]
+(defn part-one [fish]
+  (count (nth (take 81 (iterate simulate-single-day fish)) 80)))
+(defn part-two [fish] 0)
+
+(defn get-fish [args]
   (->> (nth args 0)
        (slurp)
        (split-input)
@@ -20,8 +37,8 @@
 
 (defn -main [& args]
   (if (and (= (count args) 1) (file-exists (nth args 0)))
-    (let [ints (get-ints args)]
-      (println "Part 1 result: " (part-one ints))
-      (println "Part 2 result: " (part-two ints)))
+    (let [fish (get-fish args)]
+      (println "Part 1 result: " (part-one fish))
+      (println "Part 2 result: " (part-two fish)))
 
     (println "Need exactly ONE argument: an existing file name.")))
